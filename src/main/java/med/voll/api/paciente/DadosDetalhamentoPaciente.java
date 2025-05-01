5459bd6675d52386c9505c67048b66db83bc0867
cWeber <cauna_weber@estudante.sesisenai.org.br>
2025-05-01 10:43:12 -0300

    atualizando controllers

---
+++ src/main/java/med/voll/api/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,8 @@
+package med.voll.api.paciente;
+
+public record DadosDetalhamentoPaciente(Long id, String nome, String email, String telefone, String cpf, Boolean ativo) {
+
+    public DadosDetalhamentoPaciente (Paciente paciente){
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(), paciente.getAtivo());
+    }
+}
