c2a1a820585620484e5b126070563d8cdeba0bda
cWeber <cauna_weber@estudante.sesisenai.org.br>
2025-05-05 09:51:01 -0300

    tratando erros

--- src/main/java/med/voll/api/domain/medico/Medico.java
+++ src/main/java/med/voll/api/domain/medico/Medico.java
@@ -1,51 +1,55 @@
-package med.voll.api.paciente;
+package med.voll.api.domain.medico;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import med.voll.api.endereco.Endereco;
+import med.voll.api.domain.endereco.Endereco;
 
-@Table(name = "pacientes")
-@Entity(name = "Paciente")
+@Table(name = "medicos")
+@Entity(name = "Medico")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Paciente {
+public class Medico {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
-
     private String telefone;
+    private String crm;
 
-    private String cpf;
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
 
     @Embedded
     private Endereco endereco;
 
-    private Boolean ativo;
+    private boolean ativo;
 
-    public Paciente(DadosCadastroPaciente dados) {
-        this.ativo = true;
+    public Medico(DadosCadastroMedico dados) {
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
-        this.cpf = dados.cpf();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+        this.ativo = true;
     }
 
-    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
-        if (dados.nome() != null) {
+    public void atualizarInformacoes(@Valid DadosAtualizacaoMedico dados) {
+        if(dados.nome() != null) {
             this.nome = dados.nome();
         }
-        if (dados.telefone() != null) {
+        if(dados.telefone() != null) {
             this.telefone = dados.telefone();
         }
-        if (dados.endereco() != null) {
+        if(dados.endereco() != null){
             this.endereco.atualizarInformacoes(dados.endereco());
         }
 
